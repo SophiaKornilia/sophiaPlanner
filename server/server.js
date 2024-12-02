e02e788540c5d9e1df043d98320aac582dbef8a5
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-12-02 09:11:48 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -6,12 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const PORT = 3000;
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Din frontend-URL
-    methods: ["GET", "POST"], // Tillåt endast dessa metoder
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
@@ -21,17 +16,11 @@ admin.initializeApp({
   databaseURL: "http://sophiaplanner-123.firebaseapp.com",
 });
 
-// //To check if server is running katodo
-// app.get("/", (req, res) => {
-//   res.send("Server is running!");
-// });
 const registerRoute = require("./routes/register");
 app.use("/api/register", registerRoute);
 
-// //just a test katodo
-// app.get("/api/test", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
+// const loginRoute = require("./routes/login");
+// app.use("/api/login", loginRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- sophiaplanner/src/pages/LoginPage.tsx
+++ sophiaplanner/src/pages/LoginPage.tsx
@@ -1,22 +1,87 @@
 import { Header } from "../components/Header";
+import { useState } from "react";
+import { auth } from "../../config/firebas-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import "../styles/index.css";
+import { FirebaseError } from "firebase/app";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!email) {
+      alert("Email fältet är tomt!");
+      return;
+    }
+    if (!password) {
+      alert("Lösenords fältet är tomt!");
+      return;
+    }
+
+    try {
+      // Använd signInWithEmailAndPassword för att logga in användaren
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      // Visa feedback till användaren
+      alert("Inloggning lyckades! Välkommen " + user.email);
+      //katodo om user.role = teacher else:
+      navigate("/DashboardTeacher");
+      console.log("Användare inloggad:", user);
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        // Nu har TypeScript information om att det är en FirebaseError
+        console.error("Firebase error code:", error.code);
+        console.error("Firebase error message:", error.message);
+      } else {
+        // Hantera andra typer av fel
+        console.error("Unknown error:", error);
+      }
+    }
+  };
+
   return (
     <div>
       <Header />
       <div className="h-screen items-start bg-background font-sans flex justify-center">
         <form className="h-2/4 w-1/4  bg-secondary mt-[10%] flex flex-col items-center justify-center p-6 rounded-lg shadow-md">
-          <label className="mb-2 text-lg font-semibold">Användarnamn:</label>
+          <label className="mb-2 text-lg font-semibold">
+            Användarnamn/epost:
+          </label>
           <input
             className="mb-4 p-2 w-full max-w-xs border rounded-md"
             type="text"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
           ></input>
           <label className="mb-2 text-lg font-semibold">Lösenord:</label>
           <input
             className="mb-4 p-2 w-full max-w-xs border rounded-md"
             type="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
           ></input>
-          <button className="custom-button hover:bg-opacity-80">
+          <button
+            className="custom-button hover:bg-opacity-80"
+            onClick={handleLogin}
+          >
             Logga in
           </button>
         </form>
