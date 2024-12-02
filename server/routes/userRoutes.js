85ce2f8a1b72311766c1aec737a8cf9b6f854585
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-12-02 21:00:15 +0100

    register student

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const {
   registerUser,
   loginUser,
   logOut,
+  registerStudent,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -10,5 +11,5 @@ const router = express.Router();
 router.use("/register", registerUser);
 router.use("/login", loginUser);
 router.use("/logout", logOut);
-
+router.use("/registerStudent", registerStudent);
 module.exports = router;
