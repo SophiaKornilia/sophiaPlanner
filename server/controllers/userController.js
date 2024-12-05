da76f414608290dd3f8168965680044ec5a769c7
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-12-05 11:06:45 +0100

    Frontend: - Added dropdown for selecting groups in the student registration form. - Improved layout and styles for forms and the teacher dashboard. - Implemented useCallback to optimize API calls in DashboardTeacher. - Fixed bugs with student and group rendering.
    Backend:
    - Added API endpoints for creating and fetching teacher groups.
    - Improved error handling in student and group-related endpoints.
    - Validated incoming data for group creation to avoid invalid entries.
    
    General:
    - Addressed state and rendering issues in React components.
    - Refactored code to improve maintainability and readability.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -55,7 +55,46 @@ exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Använd signInWithEmailAndPassword för att logga in användaren
+    const usersRef = collection(db, "users");
+    const emailQuery = query(usersRef, where("email", "==", email));
+    const existingUser = await getDocs(emailQuery);
+
+    if (!existingUser.empty) {
+      const user = existingUser.docs[0].data();
+      return authenticateTeacher(user.email, user.name, password, res);
+    }
+
+    const studentsRef = collection(db, "students");
+    const userNameQuery = query(studentsRef, where("userName", "==", email));
+    const existingStudent = await getDocs(userNameQuery);
+
+    if (!existingStudent.empty) {
+      const student = existingStudent.docs[0].data();
+      const isPasswordValid = await bcrypt.compare(password, student.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid password" });
+      }
+      return res.status(200).json({
+        message: "Student login successful",
+        accessToken: "studentToken",
+        user: {
+          role: "student",
+          userName: student.userName,
+          name: student.name,
+        },
+      });
+    }
+    res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const authenticateTeacher = async (email, name, password, res) => {
+  try {
+    const auth = getAuth();
     const userCredential = await signInWithEmailAndPassword(
       auth,
       email,
@@ -63,34 +102,21 @@ exports.loginUser = async (req, res) => {
     );
     const user = userCredential.user;
 
-    //sparar ytterligare data i firestore
-    const userDocRef = doc(db, "users", user.uid);
-    const userSnapShot = await getDoc(userDocRef);
-    if (!userSnapShot.exists()) {
-      return res
-        .status(404)
-        .json({ message: "User data not found in Firestore" });
-    }
-
-    const userData = userSnapShot.data();
-
     const accessToken = await user.getIdToken();
 
-    res.status(201).json({
+    return res.status(201).json({
       message: "User logedin successfully!",
-      accessToken: accessToken,
+      accessToken,
       user: {
         uid: user.uid,
         email: user.email,
-        role: userData.role,
-        name: userData.name,
+        role: "teacher",
+        name: name,
       },
     });
   } catch (error) {
-    console.error("Error registering user", error);
-    res
-      .status(500)
-      .json({ message: "Failed to login user", error: error.message });
+    console.error("Error authenticating teacher:", error);
+    return res.status(401).json({ message: "Invalid email or password" });
   }
 };
 
@@ -108,9 +134,9 @@ exports.logOut = async (req, res) => {
 
 exports.registerStudent = async (req, res) => {
   try {
-    const { name, userName, password, role } = req.body;
+    const { name, userName, password, role, teacherID } = req.body;
 
-    if (!name || !userName || !role || !password) {
+    if (!name || !userName || !role || !password || !teacherID) {
       return res.status(400).json({ message: "Missing required fields" });
     }
     const studentsRef = collection(db, "students");
@@ -132,6 +158,7 @@ exports.registerStudent = async (req, res) => {
       userName,
       role,
       password: hashedPassword,
+      teacherID: teacherID,
       createdAt: new Date(),
     });
 
@@ -143,5 +170,78 @@ exports.registerStudent = async (req, res) => {
     res
       .status(500)
       .json({ message: "Failed to register user", error: error.message });
+  }
+};
+
+exports.getStudents = async (req, res) => {
+  try {
+    const { teacherId } = req.body;
+    console.log(teacherId);
+    if (!teacherId) {
+      return res.status(400).json({ message: "Teacher ID is required" });
+    }
+    const studentsRef = collection(db, "students");
+    const studentsQuery = query(
+      studentsRef,
+      where("teacherID", "==", teacherId)
+    );
+    const querySnapshot = await getDocs(studentsQuery);
+
+    const students = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return res.status(200).json({ students });
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
+
+exports.createGroup = async (req, res) => {
+  try {
+    const { groupName, teacherId } = req.body;
+
+    const groupDocRef = doc(db, "groups", groupName);
+    await setDoc(groupDocRef, {
+      groupName: groupName,
+      teacherId: teacherId,
+      createdAt: new Date(),
+    });
+
+    res
+      .status(201)
+      .json({ message: "Group registered successfully!", groupName });
+  } catch (error) {
+    console.error("Error creating group:", error.message);
+    res
+      .status(500)
+      .json({ message: "Failed to create group", error: error.message });
+  }
+};
+
+exports.getGroups = async (req, res) => {
+  try {
+    const { teacherId } = req.body;
+
+    if (!teacherId) {
+      return res.status(400).json({ message: "Teacher ID is required" });
+    }
+    const groupsRef = collection(db, "groups");
+    const groupQuery = query(groupsRef, where("teacherId", "==", teacherId));
+    const querySnapshot = await getDocs(groupQuery);
+
+    const groups = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return res.status(200).json({ groups });
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
   }
 };
--- sophiaplanner/src/pages/DashboardTeacher.tsx
+++ sophiaplanner/src/pages/DashboardTeacher.tsx
@@ -1,10 +1,118 @@
+// import { useEffect } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { AuthContext } from "../context/AuthContext";
+interface Student {
+  userName: string;
+  name: string;
+}
 
+interface Group {
+  groupName: string;
+}
 const DashboardTeacher = () => {
+  const { user } = useContext(AuthContext);
+  const [students, setStudents] = useState<Student[]>([]);
+  const [groups, setGroups] = useState<Group[]>([]);
+
+  const getStudents = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/users/getStudents",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ teacherId: user?.uid }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setStudents(data.students); // Uppdatera eleverna
+        console.log("Get students successfully!", data);
+      } else {
+        console.error("Failed to fetch students");
+      }
+    } catch (error) {
+      console.error("Hittade inga elever", error);
+    }
+  }, [user?.uid]);
+
+  const getGroups = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/users/getGroups",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ teacherId: user?.uid }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setGroups(data.groups);
+        console.log("Get groups successfully!", data);
+      } else {
+        console.error("Failed to fetch groups");
+      }
+    } catch (error) {
+      console.error("Hittade inga grupper", error);
+    }
+  }, [user?.uid]);
+
+  useEffect(() => {
+    if (user?.uid) {
+      console.log("Fetching students for user: ", user?.uid);
+      getStudents();
+      getGroups();
+    }
+  }, [user, getStudents, getGroups]);
+
   return (
     <div>
       <Header />
-      <div className="h-screen flex justify-center items-center bg-background font-sans  "></div>
+      <div className="h-screen flex justify-center items-center bg-background font-sans">
+        {/* Yttre ruta */}
+        <div className="bg-secondary h-3/4 w-3/4 flex flex-row justify-between items-center p-6 rounded-lg shadow-lg">
+          {/* Inre rutor */}
+          <div className="bg-primary h-full w-1/3 flex flex-col rounded-md mx-2">
+            <h1 className="text-white text-xl font-bold m-3 flex justify-center">
+              Mina elever
+            </h1>
+            {students ? (
+              <ul className="text-white ml-8">
+                {students.map((student) => (
+                  <li key={student.userName}>{student.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-white ml-8">Du har inga elever än...</p>
+            )}
+          </div>
+          <div className="bg-primary h-full w-1/3 flex flex-col rounded-md mx-2">
+            <h1 className="text-white text-xl font-bold m-3 flex justify-center">
+              Mina grupper
+            </h1>
+            {groups ? (
+              <ul className="text-white ml-8">
+                {groups.map((group, index) => (
+                  <li key={index}>{group.groupName}</li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-white ml-8">Du har inga grupper än...</p>
+            )}
+          </div>
+          <div className="bg-primary h-full w-1/3 flex  justify-center rounded-md mx-2">
+            <h1 className="text-white text-xl font-bold m-3">Mina utkast</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
