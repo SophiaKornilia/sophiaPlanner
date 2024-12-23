9ed3bd4939f8a6b51cb562a98b7948d7c82e4c57
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-12-23 14:18:13 +0100

    wip - lessonplans student update

--- server/functions/index.js
+++ server/functions/index.js
@@ -3,7 +3,6 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const cronjob = require("./cron/scheduledTask");
 const userRoutes = require("./routes/userRoutes");
 
 admin.initializeApp({
